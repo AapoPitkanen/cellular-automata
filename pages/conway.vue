9067b9fb1cdeb0dbf82595096b25c94a6c94c72c
Aapo Pitkänen <aapopitkanen@gmail.com>
2021-05-12 21:46:54 +0300

    Switch to sets in game of life, add reset button and current day seed manipulation

--- pages/conway.vue
+++ pages/conway.vue
@@ -1,70 +1,68 @@
 <template lang="pug">
-  v-row(no-gutters)
+  v-row
     v-col(cols=12)
       v-row
         v-col(cols="auto")
-          v-btn(@click="handleStepBackwardClick" :disabled="selectedDay === 0")
+          v-btn(@click="handleStepBackwardClick" :disabled="currentDay === 0 || !!lifeInterval")
             v-icon mdi-step-backward
         v-col(cols="auto")
-          v-btn(@click="handleStopPlaybackClick" v-if="lifeInterval")
+          v-btn(@click="handleStopPlaybackClick" v-if="!!lifeInterval")
             v-icon mdi-stop
           v-btn(@click="handleStartPlaybackClick" v-else)
             v-icon mdi-play
         v-col(cols="auto")
-          v-btn
-            v-icon mdi-pause
-        v-col(cols="auto")
-          v-btn(@click="handleStepForwardClick")
+          v-btn(@click="handleStepForwardClick" :disabled="!!lifeInterval")
             v-icon mdi-step-forward
-      v-row
+        v-col(cols="auto")
+          v-btn(@click="handleResetClick" :disabled="!!lifeInterval")
+            v-icon mdi-restore
+        v-col(cols="auto")
+          .text-button Day {{ currentDay }}
+        v-col(cols="auto")
+          .text-button Population {{ currentPopulation }}
+      v-row(no-gutters)
         v-col(cols=12 ref="konvaContainer").konva-container
           v-stage(:config="configKonva" @dragend="handleStageDragEnd" @click="handleStageClick")
             v-layer
               v-rect(v-for="(cell, index) in cells" :config="cell" :key="`cell-${index}`")
             v-layer
               v-line(v-for="(line, index) in lines" :config="line" :key="`line-${index}`")
-      //- v-row
-      //-   v-col(cols=12).conway-grid
-      //-     v-row(v-for="(count, row) in rows" no-gutters :key="`row-${row}`").conway-grid-row
-      //-       .d-flex.flex-grow-1(v-for="(count, column) in columns" :key="`cell-${column}-${row}`" @click="handleCellClick(column, row)" :class="{ 'alive': currentCellState[`${column}-${row}`] }").conway-cell
 </template>
 
 <script>
-import { debounce, omit } from 'lodash'
+import { debounce } from 'lodash'
 
 export default {
   data() {
     return {
       lifeInterval: null,
-      day: 0,
-      selectedDay: 0,
+      currentDay: 0,
       rows: 24,
       columns: 24,
-      seed: {},
-      history: [],
+      cellStates: [new Set()],
       rectWidth: 16,
       rectHeight: 16,
-      margin: 640,
+      margin: 2560,
       configKonva: {
         x: 0,
         y: 0,
         width: 0,
         height: 0,
         draggable: true
-      }
+      },
+      cellColor: 'rgba(0, 143, 17, 0.5)',
+      gridColor: '#999'
     }
   },
   computed: {
-    cellStates() {
-      return [this.seed, ...this.history]
-    },
     currentCellState() {
-      return this.cellStates[this.selectedDay]
+      return this.cellStates[this.currentDay]
+    },
+    currentPopulation() {
+      return this.currentCellState.size
     },
     cells() {
-      const cellKeys = Object.keys(this.currentCellState).filter(
-        (key) => this.currentCellState[key]
-      )
+      const cellKeys = [...this.currentCellState.values()]
       return cellKeys.map((cellKey) => {
         const [x, y] = cellKey
           .split('/')
@@ -74,7 +72,7 @@ export default {
           y,
           width: this.rectWidth,
           height: this.rectHeight,
-          fill: 'rgba(0, 143, 17, 0.5)'
+          fill: this.cellColor
         }
       })
     },
@@ -109,7 +107,7 @@ export default {
             x,
             this.configKonva.height + this.margin + Math.abs(this.configKonva.y)
           ],
-          stroke: '#999',
+          stroke: this.gridColor,
           strokeWidth: 1
         })
       }
@@ -121,7 +119,7 @@ export default {
             this.configKonva.width + this.margin + Math.abs(this.configKonva.x),
             y
           ],
-          stroke: '#999',
+          stroke: this.gridColor,
           strokeWidth: 1
         })
       }
@@ -152,12 +150,10 @@ export default {
       this.lifeInterval = null
     },
     handleStartPlaybackClick() {
-      this.day = this.cellStates.length - 1
-      this.selectedDay = this.cellStates.length - 1
+      this.cellStates = this.cellStates.slice(0, this.currentDay + 1)
       this.lifeInterval = setInterval(() => {
         this.updateCells()
-        this.day += 1
-        this.selectedDay += 1
+        this.currentDay += 1
       }, 1000 / 15)
     },
     handleStageClick({ currentTarget }) {
@@ -168,55 +164,53 @@ export default {
       const cellX = Math.floor(relativeX / this.rectWidth)
       const cellY = Math.floor(relativeY / this.rectHeight)
       const cellKey = `${cellX}/${cellY}`
-      const updatedSeed = this.seed[cellKey]
-        ? omit(this.seed, [cellKey])
-        : {
-            ...this.seed,
-            [cellKey]: true
-          }
-      this.seed = updatedSeed
+      const updatedCurrentCellState = new Set(this.currentCellState)
+      if (this.currentCellState.has(cellKey)) {
+        updatedCurrentCellState.delete(cellKey)
+      } else {
+        updatedCurrentCellState.add(cellKey)
+      }
+      this.$set(this.cellStates, this.currentDay, updatedCurrentCellState)
     },
     handleStepBackwardClick() {
-      this.selectedDay -= 1
+      this.currentDay -= 1
     },
     handleStepForwardClick() {
       this.updateCells()
-      this.day += 1
-      this.selectedDay += 1
+      this.currentDay += 1
+    },
+    handleResetClick() {
+      this.currentDay = 0
+      this.cellStates = [new Set()]
     },
     updateCells() {
-      const newCellState = {}
-      const oldAliveCells = Object.keys(this.currentCellState).filter(
-        (cellKey) => this.currentCellState[cellKey]
-      )
-      let deadCells = {}
-      oldAliveCells.forEach((cellKey) => {
+      const newCellState = new Set()
+      let deadCells = new Set()
+      for (const cellKey of this.currentCellState) {
         const surroundingCellKeys = this.getSurroundingCellKeys(cellKey)
-        deadCells = {
-          ...deadCells,
-          ...surroundingCellKeys
-        }
+        deadCells = new Set([...deadCells, ...surroundingCellKeys])
         const aliveNeighborCount = this.getAliveNeighborCount(
           surroundingCellKeys
         )
-        newCellState[cellKey] =
-          aliveNeighborCount >= 2 && aliveNeighborCount <= 3
-      })
-      const deadCellKeys = Object.keys(deadCells)
-      deadCellKeys.forEach((cellKey) => {
+        if (aliveNeighborCount >= 2 && aliveNeighborCount <= 3) {
+          newCellState.add(cellKey)
+        }
+      }
+
+      for (const cellKey of deadCells) {
         const surroundingCellKeys = this.getSurroundingCellKeys(cellKey)
         const aliveNeighborCount = this.getAliveNeighborCount(
           surroundingCellKeys
         )
-        if (aliveNeighborCount === 3 && newCellState[cellKey] === undefined) {
-          newCellState[cellKey] = true
+        if (aliveNeighborCount === 3 && !newCellState.has(cellKey)) {
+          newCellState.add(cellKey)
         }
-      })
-      this.history.push(newCellState)
+      }
+      this.cellStates.push(newCellState)
     },
     getSurroundingCellKeys(cellKey) {
       const [cellX, cellY] = cellKey.split('/').map(Number)
-      const cellKeys = {}
+      const cellKeys = new Set()
       for (let x = -1; x < 2; ++x) {
         for (let y = -1; y < 2; ++y) {
           const nextCellX = cellX + x
@@ -224,19 +218,18 @@ export default {
           const isCenter = x === 0 && y === 0
           if (!isCenter) {
             const cellKey = `${nextCellX}/${nextCellY}`
-            cellKeys[cellKey] = true
+            cellKeys.add(cellKey)
           }
         }
       }
       return cellKeys
     },
     getAliveNeighborCount(cellKeys) {
-      const cellKeyList = Object.keys(cellKeys)
-      const aliveCells = cellKeyList.map((cellKey) => {
-        return this.currentCellState[cellKey]
-      })
-      const count = aliveCells.filter(Boolean).length
-      return count
+      const cellKeyList = [...cellKeys]
+      const aliveCells = cellKeyList.filter((cellKey) =>
+        this.currentCellState.has(cellKey)
+      )
+      return aliveCells.length
     },
     handleWindowResize: debounce(function () {
       const konvaContainer = this.$refs.konvaContainer
@@ -261,28 +254,4 @@ export default {
 <style lang="sass" scoped>
 .absolute
   position: absolute
-.conway-grid
-  .conway-grid-row
-    .conway-cell
-      position: relative
-      &.alive
-        background-color: rgba(0, 143, 17, 0.4)
-      border: 1px solid #008F11
-      max-width: 48px
-      &:nth-child(even)
-        border-right: 0
-        border-left: 0
-      &:last-child
-        border-right: 1px solid #008F11
-      &::before
-        content: ''
-        padding-bottom: 100%
-        display: block
-    &:nth-child(even)
-      .conway-cell
-        border-bottom: 0
-        border-top: 0
-    &:last-child
-      .conway-cell
-        border-bottom: 1px solid #008F11
 </style>
